8e63be6b9e936af89f81bad9445e52aa64fba2a3
Matthew Tran <60111775+mtran-12@users.noreply.github.com>
2023-02-10 17:49:47 -0800

    migrate to go instead of python

--- backend/chessBattles/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChessbattlesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chessBattles'
---
+++ backend/pkg/chess/chess_board.go
@@ -0,0 +1,5 @@
+package chess
+
+type ChessBoard struct {
+	
+}
